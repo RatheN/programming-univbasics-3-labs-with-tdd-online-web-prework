7cc494bc96e8300867c99461a475dca6c098ceb3
Nathan Rathe <nprathe10@gmail.com>
2019-06-11 19:11:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 7
-second number = 3
+second_number = 3
 differnce = first_number - second_number
 product = first_number * second_number
 quotient = first_number / second_number
